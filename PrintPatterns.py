49977181b884b16ba2ff124f55d956497325e3fc
Jai-10 <71919287+Jai-10@users.noreply.github.com>
2020-09-30 19:45:54 +0530

    Removed unnecessary spaces and tweaked the comments

--- PrintPatterns.py
+++ PrintPatterns.py
@@ -1,15 +1,13 @@
-'''
-print("# # # #")
-print("# # # #")
-print("# # # #")
-print("# # # #")
-'''
-'''
-print("# ",end="")
-print("# ",end="")
-print("# ",end="")
-print("# ",end="")
-'''
+# print("# # # #")
+# print("# # # #")
+# print("# # # #")
+# print("# # # #")
+
+# print("# ",end="")
+# print("# ",end="")
+# print("# ",end="")
+# print("# ",end="")
+
 for i in range(4):
     for j in range(4):
         print("# ", end="")
@@ -28,5 +26,3 @@ for i in range(4):
     for j in range(4-i):
         print("# ", end="")
     print()
-
-print()
